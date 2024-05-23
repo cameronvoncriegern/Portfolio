67ccd20975aa963848a9f6a6ac462e52a1f50afa
cameronvoncriegern <cameronvoncriegern@trentu.ca>
2024-05-22 20:08:38 -0400

    added lighting toggle to main page

---
+++ scripts/script.js
@@ -0,0 +1,14 @@
+const a = window.matchMedia("(prefers-color-scheme: dark)");
+console.log(a.matches);
+let nav = document.getElementById("nav");
+let neontext = document.getElementsByClassName("neontext");
+
+let lightswitch = document.getElementById("lightswitch");
+
+lightswitch.addEventListener("click",()=>{
+    lightswitch.classList.toggle("off");
+for(i=0; i<neontext.length; i++){
+    neontext[i].classList.toggle("off");
+}
+    nav.classList.toggle("off");
+});
