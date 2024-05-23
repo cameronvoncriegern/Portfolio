3eb1843f79fd672596722a81b375c945d26ed696
cameronvoncriegern <cameronvoncriegern@trentu.ca>
2024-05-22 21:37:00 -0400

    added projects section

--- scripts/script.js
+++ scripts/script.js
@@ -5,10 +5,11 @@ let neontext = document.getElementsByClassName("neontext");
 
 let lightswitch = document.getElementById("lightswitch");
 
-lightswitch.addEventListener("click",()=>{
+lightswitch.addEventListener("click",()=>{   
     lightswitch.classList.toggle("off");
 for(i=0; i<neontext.length; i++){
     neontext[i].classList.toggle("off");
 }
     nav.classList.toggle("off");
+
 });
